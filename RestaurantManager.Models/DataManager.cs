d62a1c420b938524bb099b5fdb7f3e689b251e3b
PimBettonviel <pim.bettonviel@ziggo.nl>
2015-12-03 01:07:06 +0100

    Homework #1

---
+++ RestaurantManager.Models/DataManager.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RestaurantManager.Models
+{
+    public abstract class DataManager : INotifyPropertyChanged
+    {
+        protected RestaurantContext Repository { get; private set; }
+
+        public DataManager()
+        {
+            LoadData();
+        }
+
+
+        private async void LoadData()
+        {
+            this.Repository = new RestaurantContext();
+            await this.Repository.InitializeContextAsync();
+            OnDataLoaded();
+        }
+
+        protected abstract void OnDataLoaded();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if( null != PropertyChanged)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
