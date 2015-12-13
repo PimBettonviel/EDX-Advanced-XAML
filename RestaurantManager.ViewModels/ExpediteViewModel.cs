88bd8a3ceac729e8ff6665585d4c6dd1d6112243
PimBettonviel <pim.bettonviel@ziggo.nl>
2015-12-14 00:21:10 +0100

    Homework Module 5 part 2

---
+++ RestaurantManager.ViewModels/ExpediteViewModel.cs
@@ -0,0 +1,69 @@
+﻿using RestaurantManager.Extensions;
+using RestaurantManager.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System;
+
+namespace RestaurantManager.ViewModels
+{
+    public class ExpediteViewModel : ViewModel
+    {
+
+        #region Properties
+        public ICommand DeleteOrderCommand { get; set; }
+        public ICommand DeleteAllOrdersCommand { get; set; }
+
+        private ObservableCollection<Order> _orderItems = new ObservableCollection<Order>();
+        public ObservableCollection<Order> OrderItems
+        {
+            get
+            {
+                return _orderItems;
+            }
+            set
+            {
+                _orderItems = value;
+            }
+        } 
+        #endregion
+
+        public ExpediteViewModel()
+        {
+            SetDelegateCommands();
+        }
+
+        private void SetDelegateCommands()
+        {
+            DeleteOrderCommand = new DelegateCommand<Order>(DeleteOrder);
+            DeleteAllOrdersCommand = new DelegateCommand<object>(DeleteAllOrders);
+        }
+
+        protected override void OnDataLoaded()
+        {
+            foreach (var order in Repository.Orders)
+            {
+                OrderItems.Add(order);
+            }
+        }
+
+        private void DeleteOrder(Order order)
+        {
+            if( OrderItems != null && OrderItems.Contains(order))
+            {
+                OrderItems.Remove(order);
+                Repository.Orders.Remove(order);
+            }
+        }
+
+        private void DeleteAllOrders(object obj)
+        {
+            if (OrderItems != null && OrderItems.Count > 0)
+            {
+                OrderItems.Clear();
+                Repository.Orders.Clear();
+            }
+        }
+
+    }
+}
---
+++ RestaurantManager.ViewModels/OrderViewModel.cs
@@ -0,0 +1,114 @@
+﻿using RestaurantManager.Extensions;
+using RestaurantManager.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System;
+using System.Linq;
+
+namespace RestaurantManager.ViewModels
+{
+    public class OrderViewModel : ViewModel
+    {
+
+        #region Properties
+        public ICommand SubmitOrderCommand { get; set; }
+
+        public ICommand AddToOrderCommand { get; set; }
+
+
+        private string _specialRequest;
+
+        public string SpecialRequest
+        {
+            get { return _specialRequest; }
+            set { _specialRequest = value; }
+        }
+
+        private ObservableCollection<MenuItem> _menuItems = new ObservableCollection<MenuItem>();
+        public ObservableCollection<MenuItem> MenuItems
+        {
+            get
+            {
+                return _menuItems;
+            }
+            set
+            {
+                if (_menuItems != value)
+                {
+                    _menuItems = value;
+                }
+            }
+        }
+
+        private ObservableCollection<MenuItem> _currentlySelectedMenuItems = new ObservableCollection<MenuItem>();
+        public ObservableCollection<MenuItem> CurrentlySelectedMenuItems
+        {
+            get
+            {
+                return _currentlySelectedMenuItems;
+            }
+            set
+            {
+                _currentlySelectedMenuItems = value;
+            }
+        }
+
+        #endregion
+
+        public OrderViewModel()
+        {
+            SetDelegateCommands();
+        }
+
+        protected override void OnDataLoaded()
+        {
+            foreach(var item in Repository.StandardMenuItems)
+            {
+                MenuItems.Add(item);
+            }
+
+        }
+
+        private void SetDelegateCommands()
+        {
+            SubmitOrderCommand = new DelegateCommand<object>(SaveOrder);
+            AddToOrderCommand = new DelegateCommand<MenuItem>(AddToOrder);
+        }
+
+        private void AddToOrder(MenuItem menuItem)
+        {
+            if (menuItem != null)
+            {
+                CurrentlySelectedMenuItems.Add(menuItem);
+            }
+        }
+
+        private async void SaveOrder(object obj)
+        {
+            if (CurrentlySelectedMenuItems != null && CurrentlySelectedMenuItems.Count > 0)
+            {
+                var newOrderID = Repository.Orders
+                                .Select( o => o.Id )
+                                .DefaultIfEmpty(0)
+                                .Max() + 1;
+                base.Repository.Orders.Add(new Order()
+                {
+                    Id = newOrderID,
+                    Items = new List<MenuItem>( CurrentlySelectedMenuItems),
+                    SpecialRequests = SpecialRequest
+                });
+
+                //Clear properties
+                SpecialRequest = string.Empty;
+                CurrentlySelectedMenuItems.Clear();
+                await new DialogMessageService("New order submitted.", "Submit").ShowAsync();
+            }
+            else
+            {
+                await new DialogMessageService("Please add at least 1 item to the order.", "Submit").ShowAsync();
+            }
+        }
+
+    }
+}
